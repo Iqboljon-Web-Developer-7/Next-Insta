6ce209cb1fbcae8218fc7985c08346427d83d069
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-10-13 12:27:31 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [""], // img website name to work in next app
-  },
-};
+const nextConfig = {};
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
