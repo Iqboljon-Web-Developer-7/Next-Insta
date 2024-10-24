60356244403f1c4afed426160bffc133448de0e0
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-10-24 21:29:59 +0500

    improved

--- src/components/auth/registerForm/Form.tsx
+++ src/components/auth/registerForm/Form.tsx
@@ -19,114 +19,77 @@ import { useRouter } from "next/navigation";
 import { toast } from "react-toastify"; // Assuming you want to show notifications
 import { useRegisterUserMutation } from "@/redux/api/auth/register"; // Adjust this import based on your setup
 
-import { ChangeEvent, useState } from "react";
-import { Eye, EyeClosed } from "lucide-react";
-import { useUploadFilesMutation } from "@/redux/api/Post";
-
 const formSchema = z.object({
-  fullName: z.string().min(6).max(30),
-  username: z.string().min(4).max(50),
-  email: z.string().min(4).max(50),
-  photo: z.instanceof(Image).optional(),
+  firstname: z.string().min(2).max(50),
+  lastname: z.string().min(2).max(50),
+  username: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
   password: z.string().min(2).max(50),
 });
 
-import { useToast } from "@/components/ui/useToast";
-
 export function ProfileForm() {
-  const { toast } = useToast();
   const router = useRouter();
-
   const [registerUser, { isLoading }] = useRegisterUserMutation();
-
-  const [uploadFiles, { isLoading: isUploading }] = useUploadFilesMutation();
-  const [uploadedPhoto, setUploadedPhoto] = useState("");
-
-  const [isDisabled, setIsDisabled] = useState(true);
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
-  const [showPassword, setShowPassword] = useState(false);
-  const toggleShowPassword = () => setShowPassword((p) => !p);
-
   function onSubmit(values: z.infer<typeof formSchema>) {
     const changedData = {
-      full_name: values.fullName,
+      full_name: `${values.firstname} ${values.lastname}`,
       username: values.username,
       email: values.email,
       password: values.password,
-      photo: uploadedPhoto,
+      photo: "999", // Update this if needed
     };
 
     registerUser({ body: changedData })
       .unwrap()
       .then(() => {
-        toast({
-          title: "Registered successfully!",
-          duration: 1400,
-          style: {
-            color: "#ffffff",
-            border: "none",
-            background: "#c7f9cc",
-          },
-        });
+        toast.success("Registration Successful! Please log in.");
         router.push("/auth/login");
       })
       .catch((error) => {
+        console.log(error);
         if (typeof error.data.message === "object") {
-          toast({
-            title: error.data.message[0],
-            duration: 1400,
-            style: {
-              color: "#EF4444",
-              background: "#000000",
-              borderColor: "#000000",
-            },
-          });
+          toast.error(error.data.message[0]);
         } else {
-          toast({
-            title: error.data.message,
-            duration: 1400,
-            style: {
-              color: "#EF4444",
-              background: "#000000",
-              borderColor: "#000000",
-            },
-          });
+          toast.error(error.data.message);
         }
       });
   }
-
-  const photoUploader = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files as FileList;
-
-    const formData = new FormData();
-
-    formData.append("files", files[0]);
-
-    uploadFiles({ files: formData })
-      .unwrap()
-      .then((data) => {
-        setIsDisabled(false);
-        setUploadedPhoto(data.files[0][0].url);
-      });
-  };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
           control={form.control}
-          name="fullName"
+          name="firstname"
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="text-[#EFEFEF]">Full name</FormLabel>
+              <FormLabel className="text-[#EFEFEF]">First name</FormLabel>
               <FormControl>
                 <Input
+                  placeholder="firstname"
                   {...field}
-                  className="border-none bg-[#1F1F22] rounded-sm py-6 ps-4"
+                  className="border-none bg-[#1F1F22] placeholder:text-slate-300 rounded-sm py-6 ps-4"
+                />
+              </FormControl>
+              <FormMessage className="text-red-400" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-[#EFEFEF]">Last name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="lastname"
+                  {...field}
+                  className="border-none bg-[#1F1F22] placeholder:text-slate-300 rounded-sm py-6 ps-4"
                 />
               </FormControl>
               <FormMessage className="text-red-400" />
@@ -141,8 +104,9 @@ export function ProfileForm() {
               <FormLabel className="text-[#EFEFEF]">Username</FormLabel>
               <FormControl>
                 <Input
+                  placeholder="username"
                   {...field}
-                  className="border-none bg-[#1F1F22] rounded-sm py-6 ps-4"
+                  className="border-none bg-[#1F1F22] placeholder:text-slate-300 rounded-sm py-6 ps-4"
                 />
               </FormControl>
               <FormMessage className="text-red-400" />
@@ -158,8 +122,9 @@ export function ProfileForm() {
               <FormControl>
                 <Input
                   type="email"
+                  placeholder="Email"
                   {...field}
-                  className="border-none bg-[#1F1F22]  rounded-sm py-6 ps-4"
+                  className="border-none bg-[#1F1F22] placeholder:text-slate-300 rounded-sm py-6 ps-4"
                 />
               </FormControl>
               <FormMessage className="text-red-400" />
@@ -173,42 +138,11 @@ export function ProfileForm() {
             <FormItem>
               <FormLabel className="text-[#EFEFEF]">Password</FormLabel>
               <FormControl>
-                <div className="flex items-center justify-between relative">
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    {...field}
-                    className="border-none bg-[#1F1F22] rounded-sm py-6 ps-4"
-                  />
-                  {showPassword ? (
-                    <EyeClosed
-                      onClick={toggleShowPassword}
-                      className="absolute right-3"
-                    />
-                  ) : (
-                    <Eye
-                      onClick={toggleShowPassword}
-                      className="absolute right-3"
-                    />
-                  )}
-                </div>
-              </FormControl>
-              <FormMessage className="text-red-400" />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="photo"
-          render={() => (
-            <FormItem>
-              <FormLabel className="text-[#EFEFEF]">Profile Photo</FormLabel>
-              <FormControl className="flex items-center justify-center">
                 <Input
-                  required
-                  type="file"
-                  accept="image/*"
-                  onChange={(event) => photoUploader(event)}
-                  className="pt-[.4rem] border-none text-slate-700 bg-slate-200 rounded-sm"
+                  type="password"
+                  placeholder="password"
+                  {...field}
+                  className="border-none bg-[#1F1F22] placeholder:text-slate-300 rounded-sm py-6 ps-4"
                 />
               </FormControl>
               <FormMessage className="text-red-400" />
@@ -216,17 +150,10 @@ export function ProfileForm() {
           )}
         />
         <Button
-          disabled={isDisabled}
           type="submit"
-          className="bg-[#877EFF] hover:bg-[#756bff] active:bg-[#8f87ff] w-full py-5 rounded-[.3rem] font-semibold"
+          className="bg-[#877EFF] w-full py-5 rounded-[.3rem]"
         >
-          {isUploading ? (
-            "Photo is uploading..."
-          ) : isLoading ? (
-            <div className="loader"></div>
-          ) : (
-            "Sign Up"
-          )}
+          {isLoading ? <div className="loader"></div> : "Sign Up"}
         </Button>
       </form>
     </Form>
--- src/components/util/toast/toaster.tsx
+++ src/components/util/toast/toaster.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useToast } from "@/components/hooks/use-toast";
+import { useToast } from "@/components/hooks/useToast";
 import {
   Toast,
   ToastClose,
